481fd052d572e49e33bfe0db5db03541b22989d3
Edward Linscott <edwardlinscott@gmail.com>
2020-12-22 12:00:43 +0100

    Updated python ki keywords table

--- _static/python_ki_keywords/generate_keywords.py
+++ _static/python_ki_keywords/generate_keywords.py
@@ -9,27 +9,32 @@ if __name__ == '__main__':
              '      <th>Description</th>',
              '      <th>Type</th>',
              '      <th>Default</th>',
-             '      <th>Options</th>',
              '   </tr>']
 
    with open('keywords.html', 'w') as f:
       f.write('\n'.join(header))
       for s in valid_settings:
 
+         # Parsing type
          if isinstance(s.type, (tuple, list)):
             stype = '/'.join([f'<code>{t.__name__}</code>' for t in s.type])
          else:
             stype = f'<code>{s.type.__name__}</code>'
-         if isinstance(s.options, (tuple, list)):
-            options = '/'.join([f'<code>{o}</code>' for o in s.options])
+
+         # Parsing default
+         if s.default is None:
+            default = ''
          else:
-            options = f'<code>s.options</code>'
+            default = f'<code>{s.default}</code>'
+
+         # Adding options if necessary
+         if isinstance(s.options, (tuple, list)) and s.type is not bool:
+            default += ' (must be ' + '/'.join([f'<code>{o}</code>' for o in s.options]) + ')'
 
          f.write(f'\n   <tr>')
          f.write(f'\n      <td><code>{s.name}</code></td>')
          f.write(f'\n      <td>{s.description}</td>')
          f.write(f'\n      <td>{stype}</td>')
-         f.write(f'\n      <td><code>{s.default}</code></td>')
-         f.write(f'\n      <td>{options}</td>')
+         f.write(f'\n      <td>{default}</td>')
          f.write(f'\n   </tr>')
       f.write('\n</table>')
