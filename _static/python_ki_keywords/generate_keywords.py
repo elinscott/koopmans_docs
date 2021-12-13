0b66a211591d31ea8388a1bb593c8f6c0af6ee5e
Edward Linscott <edwardlinscott@gmail.com>
2021-12-13 18:14:38 +0100

    Various modifications to align with latest version of koopmans

--- _static/python_ki_keywords/generate_keywords.py
+++ _static/python_ki_keywords/generate_keywords.py
@@ -1,48 +1,47 @@
-from koopmans.workflows.generic import valid_settings as workflow_settings
-from koopmans.calculators.ui._settings import valid_settings as ui_settings
+from koopmans.settings import WorkflowSettingsDict, UnfoldAndInterpolateSettingsDict
 
 
 def print_settings_as_html(settings, table_id, fd):
-   header = [f'<table id="{table_id}" style="width:100%; text-align:left">',
-             '   <tr>',
-             '      <th>Keyword</th>',
-             '      <th>Description</th>',
-             '      <th>Type</th>',
-             '      <th>Default</th>',
-             '   </tr>']
+    header = [f'<table id="{table_id}" style="width:100%; text-align:left">',
+              '   <tr>',
+              '      <th>Keyword</th>',
+              '      <th>Description</th>',
+              '      <th>Type</th>',
+              '      <th>Default</th>',
+              '   </tr>']
 
-   fd.write('\n'.join(header))
-   for s in settings:
+    fd.write('\n'.join(header))
+    for s in settings.settings:
 
-      # Parsing type
-      if isinstance(s.type, (tuple, list)):
-         stype = '/'.join([f'<code>{t.__name__}</code>' for t in s.type])
-      else:
-         stype = f'<code>{s.type.__name__}</code>'
+        # Parsing type
+        if isinstance(s.kind, (tuple, list)):
+            stype = '/'.join([f'<code>{t.__name__}</code>' for t in s.kind])
+        else:
+            stype = f'<code>{s.kind.__name__}</code>'
 
-      # Parsing default
-      if s.default is None:
-         default = ''
-      else:
-         default = f'<code>{s.default}</code>'
+        # Parsing default
+        if s.default is None:
+            default = ''
+        else:
+            default = f'<code>{s.default}</code>'
 
-      # Adding options if necessary
-      if isinstance(s.options, (tuple, list)) and s.type is not bool:
-         default += ' (must be ' + '/'.join([f'<code>{o}</code>' for o in s.options]) + ')'
+        # Adding options if necessary
+        if isinstance(s.options, (tuple, list)) and s.kind is not bool:
+            default += ' (must be ' + '/'.join([f'<code>{o}</code>' for o in s.options]) + ')'
 
-      fd.write(f'\n   <tr>')
-      fd.write(f'\n      <td><code>{s.name}</code></td>')
-      fd.write(f'\n      <td>{s.description}</td>')
-      fd.write(f'\n      <td>{stype}</td>')
-      fd.write(f'\n      <td>{default}</td>')
-      fd.write(f'\n   </tr>')
-   fd.write('\n</table>')
+        fd.write(f'\n   <tr>')
+        fd.write(f'\n      <td><code>{s.name}</code></td>')
+        fd.write(f'\n      <td>{s.description}</td>')
+        fd.write(f'\n      <td>{stype}</td>')
+        fd.write(f'\n      <td>{default}</td>')
+        fd.write(f'\n   </tr>')
+    fd.write('\n</table>')
 
 
 if __name__ == '__main__':
 
-   with open('workflow_keywords.html', 'w') as fd:
-      print_settings_as_html(workflow_settings, 'workflowTable', fd)
+    with open('workflow_keywords.html', 'w') as fd:
+        print_settings_as_html(WorkflowSettingsDict(), 'workflowTable', fd)
 
-   with open('ui_keywords.html', 'w') as fd:
-      print_settings_as_html(ui_settings, 'uiTable', fd)
+    with open('ui_keywords.html', 'w') as fd:
+        print_settings_as_html(UnfoldAndInterpolateSettingsDict(), 'uiTable', fd)
--- tutorials/tutorial_2/plot_bandstructure.py
+++ tutorials/tutorial_2/plot_bandstructure.py
@@ -1,16 +1,16 @@
-from koopmans import io
+from koopmans.io import read
 
 # Load the workflow object
-wf = io.read('si.kwf')
+wf = read('si.kwf')
 
 # Access the band structure from the very last calculation
-results = wf.all_calcs[-1].results
+results = wf.calculations[-1].results
 bs = results['band structure']
 
 # Print the band strucutre to file
 bs.plot(filename='ki_bandstructure.png')
 
 # Extract the band gap
-n_occ = wf.master_calcs['w90_occ'].num_wann
+n_occ = wf.master_calc_params['w90_occ'].num_wann
 gap = bs.energies[:, :, n_occ:].min() - bs.energies[:, :, :n_occ].max()
 print(f'KI band gap = {gap:.2f} eV')
